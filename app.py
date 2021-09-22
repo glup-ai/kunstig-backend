079e0db5f1be1a4dacbae09c401e40897c97909f
Målfrid Henden Aaraas <malfridha@gmail.com>
2021-09-22 21:19:22 +0200

    rename generate image function

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, Response
-from generate import generate_images2
+from generate import generate_images
 
 app = Flask(__name__)
 
 @app.route("/munch", methods = ["GET"])
 def main():
     trained_model = './trained_models/munch.pkl'
-    img = generate_images2(network_pkl=trained_model)
+    img = generate_images(network_pkl=trained_model)
     return Response(img, status=200, mimetype="image/png")

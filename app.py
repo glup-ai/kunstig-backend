597eeb0a8992473bac4f70f5ecc6019a1fcfe3ec
Målfrid Henden Aaraas <malfridha@gmail.com>
2021-09-22 18:12:06 +0200

    add todos and rename endpoint uri

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import os, random
 
 app = Flask(__name__)
 
-@app.route("/generate", methods = ["GET"])
+@app.route("/munch", methods = ["GET"])
 def main():
     seed = random.randint(1, 1000)
+    # TODO: Find a way to do this less costly
     trained_model = './trained_models/network-snapshot-010200.pkl'
     os.system(f'python generate.py --outdir=out/{seed} --trunc=1 --seeds={seed} \
     --network={trained_model}')
+    # TODO: Return image directly, don't store it
     image = random.choice(os.listdir(f'./out/{seed}'))
     return send_file(f'./out/{seed}/{image}', mimetype='image/png')

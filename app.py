b96e9cceb95c513f566042327111a0625328c581
Hege <hegehaavaldsen@gmail.com>
2021-10-18 20:14:18 +0200

    Make portrait model reachable from API

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ def main():
     trained_model = './trained_models/munch.pkl'
     img = generate_images2(network_pkl=trained_model)
     return Response(img, status=200, mimetype="image/png")
+
+@app.route("/portrait", methods = ["GET"])
+def portrait():
+    trained_model = './trained_models/portraits18.pkl'
+    img = generate_images2(network_pkl=trained_model)
+    return Response(img, status=200, mimetype="image/png")

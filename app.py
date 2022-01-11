73ad819d32a89a9d7f01e8d2254936353aade958
Max Aasbø <max.aasboe@gmail.com>
2022-01-11 21:53:45 +0100

    Add seed from input string

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response
 from generate import generate_images, load_model
 import torch
+from flask import request
 
 app = Flask(__name__)
 
@@ -19,13 +20,25 @@ def warmup():
     return "Kunstig is up and running!"
 
 
-@app.route("/munch", methods=["GET"])
+@app.route("/munch", methods=["POST"])
 def munch():
-    img = generate_images(munch_model, device)
+    json_data = request.json
+    input_string = None
+
+    if json_data:
+        input_string = json_data["inputString"]
+
+    img = generate_images(munch_model, device, input_string)
     return Response(img, status=200, mimetype="image/png")
 
 
-@app.route("/portrait", methods=["GET"])
+@app.route("/portrait", methods=["POST"])
 def portrait():
-    img = generate_images(portrait_model, device)
+    json_data = request.json
+    input_string = None
+
+    if json_data:
+        input_string = json_data["inputString"]
+
+    img = generate_images(portrait_model, device, input_string)
     return Response(img, status=200, mimetype="image/png")
--- generate.py
+++ generate.py
@@ -10,13 +10,18 @@ import legacy
 import dnnlib
 
 
-def generate_images(G, device):
+def generate_images(G, device, input_string):
     label = torch.zeros([1, G.c_dim], device=device)
 
     # Generate images.
-    seed = random.randint(1, 1000)
+    if input_string:
+        seed = generate_seed(input_string)
+    else:
+        seed = random.randint(1, 1000)
+
     z = torch.from_numpy(np.random.RandomState(seed).randn(1,
                                                            G.z_dim)).to(device)
+
     img = G(z, label, truncation_psi=1, noise_mode='const', force_fp32=True)
     img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
     image = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB')
@@ -32,3 +37,11 @@ def load_model(url, device):
     with dnnlib.util.open_url(url) as f:
         G = legacy.load_network_pkl(f)['G_ema'].to(device)
         return G
+
+
+def generate_seed(input_string):
+    print(input_string)
+    seed_size_limit = 2**32 - 1
+    seed = hash(input_string) % seed_size_limit
+    print(seed)
+    return seed

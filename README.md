e5a87f26e5a74434215ccdc1dca89eed8c20a561
Max Aasbø <max.aasboe@gmail.com>
2022-01-15 16:44:16 +0100

    Implement multi model support

--- README.md
+++ README.md
@@ -1,42 +1,109 @@
 # Kunstig (API)
+
 This repository holds Kunstig backend, a webapp that allows you to fetch images live from an AI model trained to create paintings in different art styles.
 
 This API is based on the repo [glup-ml-deployment-tutorial](https://github.com/glup-ai/glup-ml-deployment-tutorial/tree/master/Tutorial), and uses the deployment solution: Hosted app service using the Flask framework.
 
 ## Prerequisites
+
 - Python v 3.8
 
 ## Setup
+
 #### Create a new virtual environment
+
 ```
 python -m venv .venv
 ```
 
 #### Activate the environment
+
 Windows <br/>
+
 ```
 .venv/Scripts/activate
 ```
 
 Mac / Linux <br/>
+
 ```
 source .venv/bin/activate
 ```
 
 #### Install requirements
+
 ```
 pip install -r requirements.txt
 ```
 
 <a name="run-app"></a>
+
 #### Run app locally
+
 ```
 flask run
 ```
 
-#### Get images from Kunstig
-Check out the beautiful art Kunstig produces by calling on of our GET methods you can find in [app.py](https://github.com/glup-ai/kunstig-backend/blob/master/app.py):
-- `running-server/munch`
-- `running-server/portrait`
+## Usage
 
-_get the running server address from the previous step: [Run app locally](#run-app)_
+#### GET: /models
+
+Returns the complete list of models.
+
+_Response example_:
+
+```
+{
+    "models": [
+        {
+            "displayName": "Munch",
+            "name": "munch"
+        },
+        {
+            "displayName": "Portrait",
+            "name": "portrait"
+        }
+    ]
+}
+```
+
+---
+
+#### POST: /model
+
+Returns extended information about a model.
+
+_Request body_:
+
+```
+{
+    "model": string (required)
+}
+```
+
+_Response example_:
+
+```
+{
+    "displayName": "Munch",
+    "images": [
+        "test/test_image1.jpg",
+        "test/test_image2.jpg"
+    ]
+}
+```
+
+---
+
+#### POST: /generate
+
+Returns a generated image. Use the optional parameter 'inputString' to seed the image generation.
+
+_Request body_:
+
+```
+{
+    "model": string (required),
+    "inputString": string (optional)
+}
+```
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, Response
+from email.mime import image
+from pyexpat import model
+from flask import Flask, Response, jsonify
 from generate import generate_images, load_model
 import torch
 from flask import request
@@ -7,38 +9,77 @@ app = Flask(__name__)
 
 device = torch.device('cpu')
 
-munch_model = load_model(
-    'https://glupaisa.blob.core.windows.net/glup/munch.pkl', device)
+models_dict = {
+    "munch": {
+        "displayName": "Munch",
+        "url": "https://glupaisa.blob.core.windows.net/glup/munch.pkl",
+        "model": None,
+        "images": ["test/test_image1.jpg", "test/test_image2.jpg"]
+    },
+    "portrait": {
+        "displayName": "Portrait",
+        "url": "https://glupaisa.blob.core.windows.net/glup/portraits18.pkl",
+        "model": None,
+        "images": ["test/test_image1.jpg", "test/test_image2.jpg"]
+    },
+}
 
-portrait_model = load_model(
-    'https://glupaisa.blob.core.windows.net/glup/portraits18.pkl', device)
+for key in models_dict:
+    models_dict[key]["model"] = load_model(models_dict[key]["url"], device)
 
 
 @app.route("/", methods=["GET"])
 def warmup():
-    generate_images(munch, device)
     return "Kunstig is up and running!"
 
 
-@app.route("/munch", methods=["POST"])
-def munch():
+@app.route("/models", methods=["GET"])
+def models():
+
+    models = []
+
+    for key, value in models_dict.items():
+        models.append({"name": key, "displayName": value["displayName"]})
+
+    return jsonify({"models": models})
+
+
+@app.route("/model", methods=["POST"])
+def model():
     json_data = request.json
+
+    if not json_data or not "model" in json_data:
+        return Response("Missing payload", status=400)
+
+    model_name = json_data["model"]
+
+    if not model_name or not model_name in models_dict:
+        return Response("Provided model name not found", status=400)
+
+    return jsonify({
+        "displayName": models_dict[model_name]["displayName"],
+        "images": models_dict[model_name]["images"]
+    })
+
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    json_data = request.json
+
+    if not json_data or not "model" in json_data:
+        return Response("Missing payload", status=400)
+
+    model_name = json_data["model"]
+
+    if not model_name or not model_name in models_dict:
+        return Response("Provided model name not found", status=400)
+
     input_string = None
 
-    if json_data:
+    if "inputString" in json_data:
         input_string = json_data["inputString"]
 
-    img = generate_images(munch_model, device, input_string)
-    return Response(img, status=200, mimetype="image/png")
+    img = generate_images(models_dict[model_name]["model"], device,
+                          input_string)
 
-
-@app.route("/portrait", methods=["POST"])
-def portrait():
-    json_data = request.json
-    input_string = None
-
-    if json_data:
-        input_string = json_data["inputString"]
-
-    img = generate_images(portrait_model, device, input_string)
     return Response(img, status=200, mimetype="image/png")
